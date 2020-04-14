fb185a06d6e1e968f7cb7b79e46366c3de8e2748
garedb <garedbarney@gmail.com>
2020-04-14 16:43:14 -0700

    file upload site linked to cloudinary

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+require('dotenv').config()
 
-app.set('view engine', 'ejs');
-app.use(ejsLayouts);
+let express = require('express')
+let ejsLayouts = require('express-ejs-layouts')
+let app = express();
+let multer = require('multer')
+let upload = multer({ dest: './uploads' })
+let cloudinary = require('cloudinary')
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+cloudinary.config(process.env.CLOUDINARY_URL)
+let imgUrl = cloudinary.url('pwnviyhlghchtkrdksdw', {width: 200, height: 200})
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {image: imgUrl});
 });
+
+app.post('/', upload.single('myFile'), (req, res) => {
+	cloudinary.uploader.upload(req.file.path, (result) => {
+		res.send(result)		
+	})
+})
 
 app.listen(3000);
